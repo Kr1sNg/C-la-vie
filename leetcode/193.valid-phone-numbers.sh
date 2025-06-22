742893b9d5b07ef09d110dc37c193c4f377749e5
Kr1sNg <piq.k0olz@gmail.com>
2025-06-22 23:36:34 +0200

    leetcode 193

---
+++ leetcode/193.valid-phone-numbers.sh
@@ -0,0 +1,30 @@
+# Given a text file file.txt that contains a list of phone numbers (one per line), write a one-liner bash script to print all valid phone numbers.
+
+# You may assume that a valid phone number must appear in one of the following two formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit)
+
+# You may also assume each line in the text file must not contain leading or trailing white spaces.
+
+# Example:
+
+# Assume that file.txt has the following content:
+
+# 987-123-4567
+# 123 456 7890
+# (123) 456-7890
+# Your script should output the following valid phone numbers:
+
+# 987-123-4567
+# (123) 456-7890
+
+#!bin/bash
+# File containing the phone numbers
+FILE="$1"
+if [[ -z "$FILE" ]];
+then echo "Usage: $0 <filename>"; exit 1
+fi
+# Regular expression to match valid phone numbers
+REGEX='^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$'
+# Extract valid phone numbers using grep
+grep -E "$REGEX" "$FILE"
+
+
