6de8cf9873e0b9b11285a25313e6ea989b6e6183
Tat Hoang Nguyen <tat-nguy@c3r5p5.42perpignan.fr>
2025-07-11 08:45:27 +0200

    try

---
+++ 42exam/Exam-Rank-05/NewExam/vbc/vbc.c
@@ -0,0 +1,20 @@
+#include "vcb.h"
+
+bool ft_check_token(char *av)
+{
+    int i = 0;
+    while (av[i + 1])
+    {
+        if ()
+    }
+}
+
+
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        exit(1);
+    ft_parsing(av[1]);
+
+}
---
+++ 42exam/Exam-Rank-05/NewExam/vbc/vbc.h
@@ -0,0 +1,17 @@
+#ifndef _VBC_
+# define _VBC_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <boolean.h>
+
+typedef struct  s_numsym
+{
+    int     num;
+    char    
+}   t_num
+
+
+
+#endif
